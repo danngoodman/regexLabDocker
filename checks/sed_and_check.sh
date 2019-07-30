d0d5f05b38042401a814bf33420eda30206b8f2d
danngoodman <32649969+danngoodman@users.noreply.github.com>
2019-07-30 12:27:43 -0500

    Update sed_and_check.sh

--- checks/sed_and_check.sh
+++ checks/sed_and_check.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 checks_dir="/bin/checks"
-sedSolution=$(sed "$@")
+sedSolution=$(\sed "$@")
 echo "${sedSolution}"
 echo "$0 $*" >> "${checks_dir}/command_history"
 md5sum=$(echo "${sedSolution}" | cat -A | md5sum | cut -d' ' -f1)
 oldFile=$(cat "${checks_dir}/checklist")
-sed 's/'"${md5sum}"' incomplete/'"${md5sum}"' complete/' "${checks_dir}/checklist" -i 2> /dev/null
+\sed 's/'"${md5sum}"' incomplete/'"${md5sum}"' complete/' "${checks_dir}/checklist" -i 2> /dev/null
 newFile=$(cat "${checks_dir}/checklist")
 if [[ "${newFile}" != "${oldFile}" ]]
 then
   echo -ne "\n\e[34mCongrats on completing "
-  diff --new-line-format="%L" --unchanged-line-format= --old-line-format= <(echo "${oldFile}") <(echo "${newFile}") | sed 's/^\[\([^]]*\)\].*$/\1/' | tr -d '\n'
+  diff --new-line-format="%L" --unchanged-line-format= --old-line-format= <(echo "${oldFile}") <(echo "${newFile}") | \sed 's/^\[\([^]]*\)\].*$/\1/' | tr -d '\n'
   echo -e "!\e[39m"
 fi
 "${checks_dir}/send_update.sh"
