ccb662995abc2bf37e061fd55b311cdba22f08c7
U-WHQ_NT_DOMAINDG044579 <DG044579@W1562311.northamerica.cerner.net>
2019-07-30 08:35:31 -0500

    Added latest files

--- alias.sh
+++ alias.sh
@@ -1,3 +1,8 @@
 root_dir=$(pwd)
 alias grep="${PREPREFIX}/home/DG044579/regexLabDocker/checks/grep_and_check.sh"
 alias sed="${PREPREFIX}/home/DG044579/regexLabDocker/checks/sed_and_check.sh"
+alias awk="${PREPREFIX}/home/DG044579/regexLabDocker/checks/awk_and_check.sh"
+alias lab01="cd ${PREPREFIX}/home/DG044579/regexLabDocker/lab01-grep_line_matching"
+alias lab02="cd ${PREPREFIX}/home/DG044579/regexLabDocker/lab02-grep_wildcards_groups"
+alias lab03="cd ${PREPREFIX}/home/DG044579/regexLabDocker/lab03-sed_substitution"
+alias lab04="cd ${PREPREFIX}/home/DG044579/regexLabDocker/lab04-awk_data_manipulation"
