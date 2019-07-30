d0c26f76fdd0f9d7e6efb6748da5c4130014d0b7
danngoodman <32649969+danngoodman@users.noreply.github.com>
2019-07-30 12:28:17 -0500

    Update awk_and_check.sh

--- checks/awk_and_check.sh
+++ checks/awk_and_check.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 checks_dir="/bin/checks"
-awkSolution=$(awk "$@")
+awkSolution=$(\awk "$@")
 echo "${awkSolution}"
 echo "$0 $*" >> "${checks_dir}/command_history"
 md5sum=$(echo "${awkSolution}" | cat -A | md5sum | cut -d' ' -f1)
 oldFile=$(cat "${checks_dir}/checklist")
-sed 's/'"${md5sum}"' incomplete/'"${md5sum}"' complete/' "${checks_dir}/checklist" -i 2> /dev/null
+\sed 's/'"${md5sum}"' incomplete/'"${md5sum}"' complete/' "${checks_dir}/checklist" -i 2> /dev/null
 newFile=$(cat "${checks_dir}/checklist")
 if [[ "${newFile}" != "${oldFile}" ]]
 then
   echo -ne "\n\e[34mCongrats on completing "
-  diff --new-line-format="%L" --unchanged-line-format= --old-line-format= <(echo "${oldFile}") <(echo "${newFile}") | sed 's/^\[\([^]]*\)\].*$/\1/' | tr -d '\n'
+  diff --new-line-format="%L" --unchanged-line-format= --old-line-format= <(echo "${oldFile}") <(echo "${newFile}") | \sed 's/^\[\([^]]*\)\].*$/\1/' | tr -d '\n'
   echo -e "!\e[39m"
 fi
 "${checks_dir}/send_update.sh"
